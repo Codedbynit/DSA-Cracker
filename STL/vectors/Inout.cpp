e1dc9e3f235ee6dd3ff57b7ee1b6259b4c06dea6
Nityansh chandoriya <nityanshchandoriya@gmail.com>
2026-07-12 15:09:48 +0530

    created vector file

---
+++ STL/vectors/Inout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vectors>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.push_back(2);
+    v.emplace_back(3);
+
+    cout<<v[i]<<endl;
+
+    return 0;
+
+}
---
+++ STL/vectors/Syntax.cpp
@@ -0,0 +1,17 @@
+// Basic Syntax of Vectors 
+
+#include <iostream>
+#include <vectors>
+using namespace std;
+
+int main(){
+    vector<int> v; // declarations of vector
+    vector<int> v(2,10); // (2,10) ---->  vector of size 2 and storing 10 in all size
+    vector<int , int > v; // vector storing two elements in one place , in short pairs
+    vector<pair <int , int>> v; // vectors in which elements are stored in pairs
+
+    // v(5, 100) it says vector of size 5 with element 100 
+
+    
+
+}
