9fc4166d049a0c17a2880abba0b400190c41cfdc
Nityansh chandoriya <nityanshchandoriya@gmail.com>
2026-07-16 15:52:09 +0530

    Reversing number using number extraction

--- STL/vectors/Basic-maths.cpp
+++ STL/vectors/Basic-maths.cpp
@@ -1,6 +1,9 @@
 // using number extraction
 
+#include <iostream>
+using namespace std;
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -17,4 +20,23 @@ int main(){
     cout<<"Count of Number is : "<<count;
 
     return 0;
+}
+
+*/
+// Reversing number using number extraction
+
+int main(){
+    int revNum = 0;
+    int N;
+    cin>>N;
+
+    while(N>0){
+        int ld = N%10;
+        revNum = (revNum * 10)+ld;
+        N = N/10;
+    }
+    cout<<"Reversed Number is : "<<revNum;
+
+    return 0;
+
 }
